98252dacca1cbfed84d0c3b795348f6ab92c8fa8
qhoncheh <ghoncheh.ataee@gmail.com>
2025-05-03 15:04:45 +0330

    changed

--- src/components/Header/header.tsx
+++ src/components/Header/header.tsx
@@ -6,28 +6,18 @@ import { Button } from "../custom";
 export default function Header() {
   return (
     <>
-      <div className="lg:hidden">
-      </div>
+      <div className="lg:hidden"></div>
       <aside className="hidden lg:flex items-center gap-4">
         <Button className="bg-[#F0F0F0] text-black h-12 flex">
           <div className="rounded-full bg-black w-2 h-2"></div>
           خانه
         </Button>
-        <Button className="bg-[#F0F0F0] text-black h-12">
-          مقالات
-        </Button>
-        <Button className="bg-[#F0F0F0] text-black h-12">
-          درباره آلفا
-        </Button>
+        <Button className="bg-[#F0F0F0] text-black ">مقالات</Button>
+        <Button className="bg-[#F0F0F0] text-black w-28">درباره آلفا</Button>
       </aside>
       <Image src={Logo.src} alt="Logo" width={78} height={78} />
       <aside className="hidden lg:flex items-center gap-4">
-        <Button className="bg-[#7575FE] text-white h-12">
-          ثبت نام / ورود
-        </Button>
-      </aside>
-      <aside className="lg:hidden">
-        <Button className="bg-[#7575FE] text-white h-12">
+        <Button className="bg-[#7575FE] text-white w-32 ">
           ثبت نام / ورود
         </Button>
       </aside>
--- src/components/custom/Button/button.tsx
+++ src/components/custom/Button/button.tsx
@@ -4,7 +4,7 @@ import { CustomButtonProps } from "../../../../types/types";
 const Button = ({ children, ...props }: CustomButtonProps) => {
 
   return (
-    <button className={`btn btn-gray-100 border-0 rounded-3xl w-20 h-12 text-lg ${props.className}`}>
+    <button className={`btn btn-gray-100 border-0 rounded-3xl w-20 h-12 text-sm ${props.className}`}>
       {children}
     </button>
   );
