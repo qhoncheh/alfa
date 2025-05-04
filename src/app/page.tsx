8ee41b882368614b1179174add6bf225302c4998
qhoncheh <ghoncheh.ataee@gmail.com>
2025-05-04 10:48:21 +0330

    ..

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,13 @@
-
+"use client";
+import { usePathname, useRouter } from "next/navigation";
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <div>PAge</div>
-    </div>
-  );
+  const pathname = usePathname();
+  const router = useRouter();
+  if (pathname === "/") {
+    router.push(
+      "/landing"
+    );
+  }
+
+  return null;
 }
