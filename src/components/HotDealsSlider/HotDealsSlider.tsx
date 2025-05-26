593f85e4e6e94e7bb14dab8dbbe04627f4e2d9e7
aeinmananni <mananniaein1234@gmail.com>
2025-05-26 12:50:17 +0330

    update new Chnage

--- src/components/HotDealsSlider/HotDealsSlider.tsx
+++ src/components/HotDealsSlider/HotDealsSlider.tsx
@@ -8,7 +8,7 @@ export default function HotDealsSlider() {
         <span className="text-2xl">خرید و فروش های داغ این هفته</span>
         <Button className="w-max bg-[#7575FE]">مشاهده همه</Button>
       </div>
-      <div className="grid grid-cols-4 gap-14 items-center justify-center w-full overflow-hidden ">
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-14 items-center justify-center w-full overflow-hidden ">
         {HOT_DEALS_ITEMS.map((it, index) => (
           <Carts key={index} {...it} currentIndex={index} />
         ))}
